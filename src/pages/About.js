8029ff2bf143922b521c76ee025c3229e12c770d
Henry <henry.li@sesame.org>
2022-08-08 07:41:03 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        corrupti temporibus quidem, incidunt velit asperiores, repudiandae nihil
+        molestiae voluptas quasi aliquid alias culpa, voluptate repellendus qui
+        ipsa facere harum error. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Doloremque corrupti temporibus quidem, incidunt velit
+        asperiores, repudiandae nihil molestiae voluptas quasi aliquid alias
+        culpa, voluptate repellendus qui ipsa facere harum error.
+      </p>
+      <h1 className="section-title">Our Goal</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        corrupti temporibus quidem, incidunt velit asperiores, repudiandae nihil
+        molestiae voluptas quasi aliquid alias culpa, voluptate repellendus qui
+        ipsa facere harum error. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Doloremque corrupti temporibus quidem, incidunt velit
+        asperiores, repudiandae nihil molestiae voluptas quasi aliquid alias
+        culpa, voluptate repellendus qui ipsa facere harum error. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Doloremque corrupti
+        temporibus quidem, incidunt velit asperiores, repudiandae nihil
+        molestiae voluptas quasi aliquid alias culpa, voluptate repellendus qui
+        ipsa facere harum error. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Doloremque corrupti temporibus quidem, incidunt velit
+        asperiores, repudiandae nihil molestiae voluptas quasi aliquid alias
+        culpa, voluptate repellendus qui ipsa facere harum error.
+      </p>
+    </section>
+  );
+};
+
+export default About;
